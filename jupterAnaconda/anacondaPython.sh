e1f18f52fa9636f670e6aac12c5e25417198e3e7
Luiz Sabiano <luizsabiano@gmail.com>
2021-01-09 20:10:03 -0300

    jupterAnaconda/anacondaPython.sh

---
+++ jupterAnaconda/anacondaPython.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir="$(pwd)/notebooks" 
+
+### Check for dir, if not found create it using the mkdir ##
+[ ! -d "$dir" ] && mkdir -p "$dir"
+
+docker run --name python-dev -i -t -p 8888:8888 -v "$dir:/home" -w "/home" continuumio/anaconda3 /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/home --ip='*' --port=8888 --no-browser"
+docker rm $(docker ps -aq)
