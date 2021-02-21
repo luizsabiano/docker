92854d18d1bcc894fe3a8c393f35b2fec3ef5042
Luiz Sabiano Ferreira Medeiros <luizsabiano@gmail.com>
2021-02-21 19:15:59 -0300

    adicionando Framework Django

--- jupterAnaconda/anacondaPython.sh
+++ jupterAnaconda/anacondaPython.sh
@@ -6,5 +6,5 @@ dir="$(pwd)/notebooks"
 ### Check for dir, if not found create it using the mkdir ##
 [ ! -d "$dir" ] && mkdir -p "$dir"
 
-docker run --name python-dev -i -t -p 8888:8888 -v "$dir:/home" -w "/home" continuumio/anaconda3 /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/home --ip='*' --port=8888 --no-browser"
+docker run --name python-dev -i -t -p 8888:8888 -v "$dir:/home" -w "/home" continuumio/anaconda3 /bin/bash -c "/opt/conda/bin/conda install jupyter -y --quiet && pip install django && mkdir /opt/notebooks && /opt/conda/bin/jupyter notebook --allow-root --notebook-dir=/home --ip='*' --port=8888 --no-browser"
 docker rm $(docker ps -aq)
